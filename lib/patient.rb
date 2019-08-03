23b1cf3a0646c1d00af087fa18997454b3f81e6f
Alice Brunel <alc.brunel@gmail.com>
2019-08-03 18:31:10 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Patient 
+
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def new_appointment 
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    
+  end
+end
