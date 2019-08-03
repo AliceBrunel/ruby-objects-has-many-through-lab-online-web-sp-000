6239f2b9e725f13cb3c4bf7b03dbceaf0a8d52b7
Alice Brunel <alc.brunel@gmail.com>
2019-08-03 18:27:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,28 @@
 class Doctor 
+  attr_accessor :name, :appointments
 
   @@all = []
   
   def initialize(name)
-    @name = namee
+    @name = name
+    @appointment = []
+    @@all << self
   end
 
+  def self.all 
+    @@all 
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments 
+    self.appointments.each { |appointment| appointment.doctor == self }
+  end
+  
+  def patients 
+    self.appointments.collect {
+  end
+  
 end
