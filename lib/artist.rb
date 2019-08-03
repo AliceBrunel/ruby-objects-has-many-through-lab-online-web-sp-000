77a9c63be9338406df984be7d6b6f4f5c7552d48
Alice Brunel <alc.brunel@gmail.com>
2019-08-03 16:26:36 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+    song.artist = self
+  end
+
+end
